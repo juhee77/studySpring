0e764966793036735708d6e0e021171107ab397c
juhee park <jouhi3322@gmail.com>
2022-07-21 00:34:52 +0900

    mvc -api

--- src/main/java/com/example/studySpring/Controller/HelloController.java
+++ src/main/java/com/example/studySpring/Controller/HelloController.java
@@ -3,15 +3,51 @@ package com.example.studySpring.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
     @GetMapping("hello")
-    public String hello(Model model){
-        model.addAttribute("data","hello!!");
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!");
         return "hello";
     }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name; //"hello spring"
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello { //static 이용하면 내부에도 선언 가능
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
 }
 //string boot devtools이용시 html만 컴파일 하고 사용할 수 있는 기능도 있다.
 //터미널에서 스프링 서버 죽일때 command+c
